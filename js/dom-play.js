d766092fad269b1f4dd4323e650f15bb58e174f0
FanielHabte <fanielhabte20@gmail.com>
2023-06-28 23:44:04 -0700

    update

---
+++ js/dom-play.js
@@ -0,0 +1,15 @@
+//alert("Hi from dom play");
+/*
+
+    Creat an app that on click actpors lines are highlighted 
+
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play 
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
+
+    The function(s) created to do the work must accommodate any number of actors and any number of additional lines added to the page.
+
+    HTML "hooks" may be added to the page for proper operations 
+
+*/
+
+//document.querySelector(".Hamlet").style.backgroundColor = " Yellow"
